0810e99a0f816b0521648550075239fd56af0c80
vladyslawww <gv810528@gmail.com>
2021-04-23 16:54:12 +0300

    Base module started

--- base/src/main/kotlin/com/holikov/base/base/list/paging/PagingListener.kt
+++ base/src/main/kotlin/com/holikov/base/base/list/paging/PagingListener.kt
@@ -1,2 +1,19 @@
 package com.holikov.base.base.list.paging
 
+import androidx.recyclerview.widget.RecyclerView
+import java.util.concurrent.atomic.AtomicReference
+
+typealias PagingAdapter = () -> Unit
+
+class PagingListener(private val pageSize: Int = 15) : RecyclerView.OnScrollListener() {
+
+    private val isLoading = AtomicReference(false)
+
+    private val isRegistered = AtomicReference(false)
+
+    private val listCallBack = PagingListCallback(isLoading)
+
+    private val pageListener = AtomicReference<PagingAdapter>()
+
+    private val currentScroll = AtomicReference(0)
+}
--- base/src/main/kotlin/com/holikov/base/di/KodeinViewModelProvider.kt
+++ base/src/main/kotlin/com/holikov/base/di/KodeinViewModelProvider.kt
@@ -1,4 +1,32 @@
 package com.holikov.base.di
 
-class KodeinViewModelProvider {
+import android.text.Editable
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentActivity
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+
+class KodeinViewModelProvider private constructor() {
+
+    companion object {
+        inline fun <reified T : ViewModel> of(fragment: Fragment, crossinline factory: () -> T): T {
+
+            @Suppress("UNCHECKED_CAST")
+            val vmFactory = object : ViewModelProvider.Factory {
+                override fun <U : ViewModel> create(modelClass: Class<U>): U = factory() as U
+            }
+
+            return ViewModelProvider(fragment, vmFactory)[T::class.java]
+        }
+
+        inline fun <reified T : ViewModel> of(fragmentActivity: FragmentActivity, crossinline factory: () -> T): T {
+
+            @Suppress("UNCHECKED_CAST")
+            val vmFactory = object : ViewModelProvider.Factory {
+                override fun <U : ViewModel> create(modelClass: Class<U>): U = factory() as U
+            }
+
+            return ViewModelProvider(fragmentActivity, vmFactory)[T::class.java]
+        }
+    }
 }
